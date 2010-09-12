0cbe9d3032af33b01807d4332fb3238ce809ef9d
Piotr Gabryjeluk <piotr@wikidot.com>
2010-09-12 22:41:42 +0200

    Added maintainer info to deb

--- build-simple-deb.sh
+++ build-simple-deb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd "`dirname "$0"`"
-make && checkinstall -y --nodoc --pkggroup web --requires libqtwebkit4 cp ffbrowser /usr/bin
+make && checkinstall -y --maintainer "Piotr Gabryjeluk <piotr@gabryjeluk.pl>" --nodoc --pkggroup web --requires libqtwebkit4 cp ffbrowser /usr/bin
 
